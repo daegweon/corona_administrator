a2a19d7dcb789435a168a4156e11c90b1550caca
forcallinghenney <godisbest101@gmail.com>
2020-03-30 18:53:53 +0900

    add my_inform dialog

--- app/src/main/java/com/example/corona_administrator/LoginActivity.java
+++ app/src/main/java/com/example/corona_administrator/LoginActivity.java
@@ -7,57 +7,72 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
 
 public class LoginActivity extends AppCompatActivity {
+    private String name, id, pw, phone_num;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button login_btn = findViewById(R.id.login_btn);
+        login_btn.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                name = ((EditText)findViewById(R.id.name_editText)).getText().toString();
+                id = ((EditText)findViewById(R.id.id_editText)).getText().toString();
+                pw = ((EditText)findViewById(R.id.pw_editText)).getText().toString();
+                phone_num = ((EditText)findViewById(R.id.phone_editText)).getText().toString();
+
+
+                if (name.isEmpty() || id.isEmpty() || pw.isEmpty() || phone_num.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(), "로그인 정보를 전부 입력해주세요", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                if(isValidUser())
+                {
+                    Intent managing_activity = new Intent(getApplicationContext(), ManagingActivity.class);
+
+                    managing_activity.putExtra("name", name);
+                    managing_activity.putExtra("id", id);
+                    managing_activity.putExtra("phone_num", phone_num);
+
+                    startActivity(managing_activity);
+                    finish();
+                }
+
+                else
+                {//fail to log in
+                    AlertDialog.Builder login_fail = new AlertDialog.Builder(getApplicationContext());
+                    login_fail.setTitle("로그인 실패");
+                    login_fail.setMessage("로그인에 실패하였습니다. 입력하신 정보를 확인해주세요");
+                    login_fail.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+
+                    });
+
+                    login_fail.show();
+                }
+            }
+        });
     }
 
-    public void onLoginButtonClicked(View view){
-        String name = ((EditText)findViewById(R.id.name_editText)).getText().toString();
-        String id = ((EditText)findViewById(R.id.id_editText)).getText().toString();
-        String pw = ((EditText)findViewById(R.id.pw_editText)).getText().toString();
-        String phone_num = ((EditText)findViewById(R.id.phone_editText)).getText().toString();
-
-
-        if (name.isEmpty() || id.isEmpty() || pw.isEmpty() || phone_num.isEmpty())
-        {
-            Toast.makeText(this, "로그인 정보를 전부 입력해주세요", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        else
-        {
-            if(isValidUser(id, pw))
-            {
-                Intent managing_activity = new Intent(this, ManagingActivity.class);
-                startActivity(managing_activity);
-            }
-            else
-            {//fail to log in
-                final AlertDialog.Builder login_fail = new AlertDialog.Builder(this);
-                login_fail.setTitle("로그인 실패");
-                login_fail.setMessage("로그인에 실패하였습니다. 입력하신 정보를 확인해주세요");
-                login_fail.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-
-                });
-
-                login_fail.show();
-            }
-        }
-    }
-
-    private boolean isValidUser(String id, String pw){
+    //수정필요
+    private boolean isValidUser(){
         return true;
     }
 }
