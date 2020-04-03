5d49d49d15e46623123b5052ca6d8e754b3c05bb
forcallinghenney <godisbest101@gmail.com>
2020-04-03 15:23:35 +0900

    lost_communication_time

--- app/src/main/java/com/example/corona_administrator/ManagingActivity.java
+++ app/src/main/java/com/example/corona_administrator/ManagingActivity.java
@@ -12,6 +12,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
