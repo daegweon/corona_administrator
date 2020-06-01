a660572ff6375d0909506214ed072bd2318e61b5
arohen24 <arohen24@gmail.com>
2020-06-01 15:24:19 +0900

    automatic register FCM token (only for test)

--- app/src/main/java/com/example/corona_administrator/messaging/PushMessagingService.java
+++ app/src/main/java/com/example/corona_administrator/messaging/PushMessagingService.java
@@ -9,11 +9,64 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 public class PushMessagingService extends FirebaseMessagingService {
     public static String ACTION_MSG_FROM_SERVER = "msg_from_server";
 
     @Override
     public void onNewToken(@NonNull String token) {
+        JSONObject uploadJSON = new JSONObject();
+
+        try {
+            uploadJSON.put("code", "TEMP_CODE");
+            uploadJSON.put("token", token);
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        HttpURLConnection conn = null;
+        try{
+            conn = (HttpURLConnection)new URL("http://143.248.53.50:8000/api/register_manager").openConnection();
+            conn.setRequestMethod("POST");
+            conn.setConnectTimeout(500);
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (conn != null) {
+            try {
+                // 서버로 전송
+                conn.connect();
+
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream(), StandardCharsets.UTF_8));
+                writer.write(uploadJSON.toString());
+                writer.flush();
+                writer.close();
+
+                conn.getOutputStream().close();
+                // 정상적으로 리턴이 돌아오면
+                if (conn.getResponseCode() == 201) {
+                    Log.d("SendAsync", "Sending data was successful");
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            } finally{
+                conn.disconnect();
+            }
+        }
+
         Log.d("PushMessagingService", "onNewToken - " + token);
     }
 
